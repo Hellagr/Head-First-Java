6c4a3fc20f1a4c5cd1e0fb6d3d284d8b1b03d289
Hellagr <98410790+Hellagr@users.noreply.github.com>
2022-03-21 20:17:51 +0300

    Update and rename drum to drum.java

---
+++ drum/drum.java
@@ -0,0 +1,16 @@
+package drum;
+
+    public class DrumKit {
+		
+	public boolean topHat = true;
+	public boolean snare = true;
+	
+	public void playSnaze () {
+	System.out.println("Bah Bah Ba-Bah");
+	}
+	public void playTopHat () {
+	System.out.println("Diz");
+	}
+	
+
+}
